01d7e7cb7a11626f9fd7fa05b78c0fa56825b7f7
AlaaMatouk <contact.alaamatouk@gmail.com>
2026-06-27 04:13:21 +0300

    add sitemap and google verification

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 import react from "@vitejs/plugin-react";
+import sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: "https://tippytap-blog.vercel.app",
+    }),
+  ],
 });
